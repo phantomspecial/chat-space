3bd8ab2751d32ad888db997e2d816ba7672f52f9
phantomspecial <win.2000@live.jp>
2017-12-08 15:54:12 +0900

    Make-Group-model-controller,Routing,ActionSetting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "messages#index"
-  resources :messages
+  root "messages#index"
+  resources :groups do
+    resources :messages
+  end
   resources :users, only: [:edit , :update]
 end
