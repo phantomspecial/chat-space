53c751cb9aa17a7fd7d3698e1b660f6c6c2b4e01
phantomspecial <win.2000@live.jp>
2017-12-10 17:01:40 +0900

    Message-Send-function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,12 +3,24 @@ class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
     @currentgroup = Group.find(params[:group_id])
-  end
 
-  def new
+    @messages = @currentgroup.messages
+    @message = current_user.messages.new
   end
 
   def create
+    @message = current_user.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@message.group_id), notice:"メッセージ送信成功"
+    else
+      redirect_to group_messages_path(@message.group_id), alert:"メッセージを入力もしくは写真を選択してください"
+    end
   end
+
+  private
+  def message_params
+    params.require(:message).permit(:content, :image).merge(group_id: params[:group_id])
+  end
+
 
 end
